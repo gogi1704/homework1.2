7092a138b85e6c0575d731fef3cf91c871eea17f
Ларионов Георгий Юрьевич <zoralarionov@gmail.com>
2022-03-27 15:42:39 +0300

    TaskTwo completed

--- src/main/kotlin/TaskTwo.kt
+++ src/main/kotlin/TaskTwo.kt
@@ -1,3 +1,13 @@
-fun main(){
+val likes = 121
+
+fun main() {
+    defineWord(likes)
+}
+
+fun defineWord(likes: Int) {
+    when (likes % 10) {
+        1 -> println("Человеку")
+        else -> println("Людям")
+    }
 
 }
