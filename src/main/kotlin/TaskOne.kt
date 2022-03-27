cc2638f3594613a0c0b4e859fc4aab56493474e3
Ларионов Георгий Юрьевич <zoralarionov@gmail.com>
2022-03-27 14:41:35 +0300

    TaskOne completed

--- src/main/kotlin/TaskOne.kt
+++ src/main/kotlin/TaskOne.kt
@@ -1,3 +1,19 @@
-fun main(){
+val commissionPercent = 0.075
+
+fun main() {
+
+    val amount = 2000
+    println("Коммисия составляет " + calculateCommission(amount, commissionPercent))
 
 }
+
+fun calculateCommission(amount: Int, commissionPercent: Double): Int {
+    var commission = amount * commissionPercent
+    if (commission < 3500) {
+        commission = 3500.0
+    }
+    val commissionInt = commission.toInt()
+    return commissionInt
+}
+
+//переводы с любых карт комиссия 0.75%, минимум 35 рублей.
